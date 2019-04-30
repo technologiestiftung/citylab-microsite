31db9835fed32467a72ea78e73f009a91a99426a
Sebastian Meier <contact@sebastianmeier.eu>
2019-04-30 16:12:04 +0200

    event pages added

---
+++ assets/events/mobilitysymposium.js
@@ -0,0 +1,51 @@
+export const content = {
+  'de':{
+    'title': 'Urban Mobility Symposium',
+    'subTitle': 'Karten, Daten, Geovisualisierung',
+    'subSubTitle': '11.10.2019 - CityLab Berlin - Flughafen Tempelhof',
+    'intro': 'Weltweit leben bereits mehr als die Hälfte aller Menschen in städtischen Räumen. Urbane Mobilität ist essentiell, ob für das Zusammenleben oder wirtschaftliche Prozesse. Der traditionelle motorisierte Verkehr ist eine wichtige Einflussgröße für Luft- und Lärmverschmutzung im urbanen Raum. Ein zunehmend diverser “Modal Split” stellt Verkehrsplaner*innen vor neue Herausforderungen. Diesen und weiteren Themen rund um die urbane Mobilität wollen wir uns im Rahmen des Urban Mobility Symposiums widmen. Wir suchen innovative Forschung, aber auch Berichte aus der Praxis. Fokus der Arbeiten sollte auf der Verarbeitung, Analyse und Darstellung räumlicher Informationen im Kontext urbaner Mobilität liegen. Von kartographischen Anwendungen und Produkten zur visuellen Analyse, Vermittlung und Darstellung von Mobilitätsdaten, bis hin zu geoinformatischen Lösungsansätzen multimodalen Routings oder Verkehrsflussoptimierungen. Ein wünschenswerter, aber nicht zwingender Fokus liegt auf Ansätzen, die sich mit neuen Innovationen für nachhaltigere Mobilitätslösungen beschäftigen.',
+    'timeTitle':'Timeline',
+    'timeline':[
+      ['30.04.','Call for Abstracts'],
+      ['01.07.','Submission Deadline'],
+      ['01.08.','Paper Acceptance'],
+      ['11.10.','Symposium'],
+      ['30.11.','Submission Short Papers for Journals']
+    ],
+    'paragraphs':[
+      { 'type':'paragraph', 'content': 'Danach folgt das Review Verfahren durch die jeweiligen Journals.<br />Geplante Publikation Frühjahr/Sommer 2020.' },
+      { 'type':'title', 'content': 'Call for Participation' },
+      { 'type':'paragraph', 'content': 'Für das Symposium können Vortragsvorschläge als Extended Abstracts (Max. 3 Seiten inkl. Abbildungen und Referenzen) in deutscher und englischer Sprache eingereicht werden. Diese werden vom Program Committee reviewt. Angenommene Vorträge bekommen einen 20 Minuten Slot zur Präsentation. Diese Abstracts werden als “Open Access” (CC BY-NC-ND) Proceedings auf der Symposiums-Website veröffentlicht. Alle angenommenen Extended Abstracts erhalten darüber hinaus die Möglichkeit, ihre Arbeit in Form eines Papers in englischer Sprache in einer Sonderausgabe der Kartographischen Nachrichten (KN) oder dem gis.Science Journal zu veröffentlichen. Wir bitten hierbei zu berücksichtigen, dass die Paper dabei einem weiteren Peer-Review Schritt durch die jeweiligen Publikationen unterzogen werden.' },
+      { 'type':'title', 'content': 'Student Track' },
+      { 'type':'paragraph', 'content': 'Wir rufen hiermit auch explizit junge Nachwuchswissenschaftler*innen dazu auf, sich am Diskurs und dem Symposium zu beteiligen. Herausragende Semesterprojekte und Bachelor- oder Masterarbeiten können ebenfalls in Form von Extended Abstracts eingereicht werden. In diesem Kontext wollen wir auch darauf hinweisen, dass eine Annahme zur Präsentation im Rahmen des Symposiums nicht bedeutet, dass man danach ein Journal-Paper veröffentlichen muss. Dies ist ein optionales Angebot.' },
+      { 'type':'paragraph', 'content': 'Die Extended Abstracts sind bis zum 01.07. als PDFs per Email an meier@technologiestiftung-berlin.de zu richten.' },
+      { 'type':'paragraph', 'content': 'Die Teilnahme am Symposium ist kostenfrei. Aufgrund der räumlichen Gegebenheiten ist die Veranstaltung auf maximal 70 Teilnehmer*innen beschränkt. Alle angenommenen Autor*innen bekommen zwei Tickets für das Symposium reserviert. Die restlichen Tickets stehen ab dem 1. August mit der Bekanntgabe der Speaker*innen zur Verfügung.' },
+      { 'type':'paragraph', 'content': 'Die Veranstaltung wird finanziell unterstützt durch die Technologiestiftung Berlin und den Regierenden Bürgermeister Berlins die Senatskanzlei.' },
+    ],
+    'programTitle': 'Program Committee',
+    'chairTitle': 'Chairs',
+    'chairs':[
+      ['Johannes Kröger','HafenCity University, Hamburg'],
+      ['Sebastian Meier','Technologiestiftung Berlin']
+    ],
+    'memberTitle':'Members',
+    'members':[
+      ['Marian Dörk','Fachhochschule Potsdam'],
+      ['Julia Gonschorek','EASC eV.'],
+      ['Anita Graser','Austrian Institute of Technology, Wien'],
+      ['Katharina Jacob','Fachhochschule Potsdam'],
+      ['Alexandra Kapp','Technologiestiftung Berlin'],
+      ['Felix Kunde','Zalando'],
+      ['Nicolas Neubauer','HERE, Berlin'],
+      ['Charlotte Pusch','Technische Universität Hamburg'],
+      ['Jochen Schiewe','HafenCity University, Hamburg'],
+      ['Harald Tschernthanner','Universität Potsdam'],
+      ['Lucia Tyrallova','Universität Potsdam'],
+      ['Raphael Reimann','Moovel'],
+      ['Sigrun Beige','Telefonica Germany - Mobility Research Group']
+    ]
+  },
+  'en':{
+    'title': 'Mobility Symposium - en'
+  }
+};
---
+++ pages/events/mobilitysymposium.vue
@@ -0,0 +1,102 @@
+<template>
+    <div>
+		<Navigation :scrolled="true" :lang="lang" :content="content" :direct="direct" :anchorTags="false"/>
+        <section class="section is-medium">
+            <div class="container">
+                <h1 class="title" style="margin-top: 100px">
+                    {{ eventContent[lang]['title'] }}
+                </h1>
+				<h2>{{ eventContent[lang]['subTitle'] }}</h2>
+				<h3>{{ eventContent[lang]['subSubTitle'] }}</h3>
+				<p class="event-intro">{{ eventContent[lang]['intro'] }}</p>
+
+				<h4>{{ eventContent[lang]['timeTitle'] }}</h4>
+				<ul>
+					<li v-for="time in timeArr">
+						<strong>{{ time[0] }}</strong>
+						<span>{{ time[1] }}</span>
+					</li>
+				</ul>
+
+				<div v-for="paragraph in parArr">
+					<h4 v-if="paragraph.type === 'title'">{{ paragraph.content }}</h4>
+					<p v-if="paragraph.type === 'paragraph'">{{ paragraph.content }}</p>
+				</div>
+
+				<h4>{{ eventContent[lang]['programTitle'] }}</h4>
+
+				<h5>{{ eventContent[lang]['chairTitle'] }}</h5>
+				<ul>
+					<li v-for="chair in chairArr">
+						<strong>{{ chair[0] }}</strong>
+						<span>{{ chair[1] }}</span>
+					</li>
+				</ul>
+
+				<h5>{{ eventContent[lang]['memberTitle'] }}</h5>
+				<ul>
+					<li v-for="member in memberArr">
+						<strong>{{ member[0] }}</strong>
+						<span>{{ member[1] }}</span>
+					</li>
+				</ul>
+
+				<Matomo/>
+            </div>
+        </section>
+		<Footer :lang="lang" :content="content"/>
+	</div>
+</template>
+
+<script>
+	import { 
+  		content as content
+	} from '../../assets/content.js';
+
+  import { 
+  		content as eventContent
+	} from '../../assets/events/mobilitysymposium.js';
+
+  import Navigation from '../../components/Navigation.vue';
+	import Footer from '../../components/Footer.vue';
+	import Matomo from '../../components/Matomo.vue';
+    
+    export default {
+        components: {
+			Navigation,
+			Footer,
+			Matomo
+		},
+		data() {
+			return {
+				lang: 'de',
+				content: content,
+        eventContent: eventContent,
+				direct: '/events/mobilitysymposium_en'
+			}
+		},
+		computed: {
+            timeArr() {
+                return this.eventContent[this.lang].timeline
+            },
+			parArr() {
+                return this.eventContent[this.lang].paragraphs
+            },
+			chairArr() {
+                return this.eventContent[this.lang].chairs
+            },
+			memberArr() {
+                return this.eventContent[this.lang].members
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+	@import "../../assets/style/style.scss";
+    h1.title, h3.title {
+		color: $color-secondary;
+	}
+</style>
+
+
