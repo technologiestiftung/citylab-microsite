16ab9f7c900e7b80bc12e486a42a67a338a7007b
Sebastian Meier <contact@sebastianmeier.eu>
2019-04-30 22:23:46 +0200

    Design Event Pages

--- pages/events/mobilitysymposium_en.vue
+++ pages/events/mobilitysymposium_en.vue
@@ -1,40 +1,43 @@
 <template>
     <div>
-		<Navigation :scrolled="true" :lang="lang" :content="content" :direct="direct" :anchorTags="false"/>
-        <section class="section is-medium">
+		<Navigation :scrolled="true" :lang="lang" :content="content" :direct="direct" :anchorTags="false" />
+		<HeroLight :image="'../images/hero_img_mobility_symposium.jpg'" :title="eventContent[lang]['title']" :subTitle="eventContent[lang]['subTitle']" :subSubTitle="eventContent[lang]['subSubTitle']" />
+        <section class="section is-medium event-content">
             <div class="container">
-                <h1 class="title" style="margin-top: 100px">
-                    {{ eventContent[lang]['title'] }}
-                </h1>
-				<h2>{{ eventContent[lang]['subTitle'] }}</h2>
-				<h3>{{ eventContent[lang]['subSubTitle'] }}</h3>
 				<p class="event-intro">{{ eventContent[lang]['intro'] }}</p>
 
-				<h4>{{ eventContent[lang]['timeTitle'] }}</h4>
-				<ul>
+				<h4 class="title">{{ eventContent[lang]['timeTitle'] }}</h4>
+				<ul class="time-list">
 					<li v-for="time in timeArr">
 						<strong>{{ time[0] }}</strong>
 						<span>{{ time[1] }}</span>
 					</li>
 				</ul>
 
-				<h4>{{ eventContent[lang]['programTitle'] }}</h4>
+				<div v-for="paragraph in parArr">
+					<h4 class="title" v-if="paragraph.type === 'title'">{{ paragraph.content }}</h4>
+					<p v-if="paragraph.type === 'paragraph'" v-html="paragraph.content"></p>
+				</div>
 
-				<h5>{{ eventContent[lang]['chairTitle'] }}</h5>
-				<ul>
+				<h4 class="title">{{ eventContent[lang]['programTitle'] }}</h4>
+
+				<h5 class="title">{{ eventContent[lang]['chairTitle'] }}</h5>
+				<ul class="name-list">
 					<li v-for="chair in chairArr">
 						<strong>{{ chair[0] }}</strong>
 						<span>{{ chair[1] }}</span>
 					</li>
 				</ul>
 
-				<h5>{{ eventContent[lang]['memberTitle'] }}</h5>
-				<ul>
+				<h5 class="title">{{ eventContent[lang]['memberTitle'] }}</h5>
+				<ul class="name-list">
 					<li v-for="member in memberArr">
 						<strong>{{ member[0] }}</strong>
 						<span>{{ member[1] }}</span>
 					</li>
 				</ul>
+
+				<p class="copyright">{{ eventContent[lang]['rights'] }}</p>
 
 				<Matomo/>
             </div>
@@ -53,12 +56,14 @@
 	} from '../../assets/events/mobilitysymposium.js';
 
   import Navigation from '../../components/Navigation.vue';
+  import HeroLight from '../../components/HeroLight.vue';
 	import Footer from '../../components/Footer.vue';
 	import Matomo from '../../components/Matomo.vue';
     
     export default {
         components: {
 			Navigation,
+			HeroLight,
 			Footer,
 			Matomo
 		},
@@ -92,6 +97,42 @@
     h1.title, h3.title {
 		color: $color-secondary;
 	}
+
+	.event-content p{
+		color: $color-tertiary;
+        font-size: $size-5;
+		padding-bottom:40px;
+	}
+
+	h4.title{
+		color: $color-secondary !important;
+		font-size: $size-3;
+		padding-bottom:20px;
+	}
+
+	h5.title{
+		color: $color-secondary !important;
+		font-size: $size-4;
+		padding-bottom:0px;
+		margin-bottom:10px !important;
+	}
+
+	.name-list li,
+	.time-list li{
+		color: $color-tertiary;
+        font-size: $size-5;
+		padding-bottom:5px;
+	}
+
+	.name-list, .time-list{
+		margin-bottom:20px;
+	}
+
+	.event-content p.copyright{
+		padding-top:40px;
+		font-size: 14px;
+	}
+
 </style>
 
 
