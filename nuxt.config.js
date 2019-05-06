f9169cdc3e3850ca42027740d3062057ac01cb78
Sebastian Meier <contact@sebastianmeier.eu>
2019-05-06 15:02:11 +0200

    title & meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,6 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: 'CityLAB',
-    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
