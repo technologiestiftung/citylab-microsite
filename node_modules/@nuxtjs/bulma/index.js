887ec1ec53eafa43a18853b5680dc3eec2e57d19
fdnklg <fabian.dinklage@me.com>
2019-06-26 13:22:12 +0200

    dist and node modules removed

--- node_modules/@nuxtjs/bulma/index.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-module.exports = function nuxtBulma(options) {
-  // Add CSS
-  this.options.css.push('bulma/css/bulma.css')
-}
-
-module.exports.meta = require('./package.json')
