c254ba34facc53f29ef8b6f6ee54455055e91954
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-06-22 15:12:37 +0200

    feat(newsletter): create unsubscribe mixin

--- mixins/subscriptionHandling.js
+++ mixins/subscriptionHandling.js
@@ -59,6 +59,60 @@ export default {
           window.open(routeData.href, "_blank");
         }
       );
+    },
+    unsubscribe(data, token) {
+      const vm = this;
+
+      !(function(e, t, n, c, r, a, i) {
+        (e.Newsletter2GoTrackingObject = r),
+          (e[r] =
+            e[r] ||
+            function() {
+              (e[r].q = e[r].q || []).push(arguments);
+            }),
+          (e[r].l = 1 * new Date()),
+          (a = t.createElement(n)),
+          (i = t.getElementsByTagName(n)[0]),
+          (a.async = 1),
+          (a.src = c),
+          i.parentNode.insertBefore(a, i);
+      })(
+        window,
+        document,
+        "script",
+        "https://static.newsletter2go.com/utils.js",
+        "n2g"
+      );
+      n2g("create", token);
+
+      n2g(
+        "unsubscribe:send",
+        {
+          recipient: data
+        },
+        function(response, message) {
+          if (response.status == 201) {
+            // unsubscribe successful
+            const routeData = vm.$router.resolve({
+              path: `/newsletter_status/unsubscribed_${vm.lang}`
+            });
+            window.open(routeData.href, "_blank");
+          } else {
+            // error while unsubscribing
+            const routeData = vm.$router.resolve({
+              path: `/newsletter_status/error_${vm.lang}`
+            });
+            window.open(routeData.href, "_blank");
+          }
+        },
+        function(data) {
+          // error while unsubscribing
+          const routeData = vm.$router.resolve({
+            path: `/newsletter_status/error_${vm.lang}`
+          });
+          window.open(routeData.href, "_blank");
+        }
+      );
     }
   }
 };
