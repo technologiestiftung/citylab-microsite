b2fbeccb4d765bc087457cbbc94c68be4dbd6d22
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-06-22 15:05:04 +0200

    feat(newsletter): avoid repetition of subscribe function

--- pages/newsletter_register_de.vue
+++ pages/newsletter_register_de.vue
@@ -11,7 +11,7 @@
         <h1 class="title" style="margin-top: 100px">
           {{ content[lang]["register"]["title"] }}
         </h1>
-        <SubscribeForm :content="formContent" @submit-data="subscribe"  />
+        <SubscribeForm :content="formContent" @submit-data="handleSubscribe"  />
       </div>
       <Matomo />
     </section>
@@ -26,8 +26,11 @@ import Footer from "../components/Footer.vue";
 import Matomo from "../components/Matomo.vue";
 import SubscribeForm from "../components/forms/SubscribeForm";
 
+import subscriptionHandling from '../mixins/subscriptionHandling.js';
+
 export default {
   name: "NewsletterRegisterDe",
+  mixins: [ subscriptionHandling ],
   components: {
     Navigation,
     Matomo,
@@ -47,70 +50,8 @@ export default {
     }
   },
   methods: {
-    subscribe(data) {
-
-      const vm = this;
-
-      !(function(e, t, n, c, r, a, i) {
-        (e.Newsletter2GoTrackingObject = r),
-          (e[r] =
-            e[r] ||
-            function() {
-              (e[r].q = e[r].q || []).push(arguments);
-            }),
-          (e[r].l = 1 * new Date()),
-          (a = t.createElement(n)),
-          (i = t.getElementsByTagName(n)[0]),
-          (a.async = 1),
-          (a.src = c),
-          i.parentNode.insertBefore(a, i);
-      })(
-        window,
-        document,
-        "script",
-        "https://static.newsletter2go.com/utils.js",
-        "n2g"
-      );
-      n2g("create", this.content[this.lang]["register"]["token"]);
-
-      n2g(
-        "subscribe:send",
-        {
-          recipient: {
-            first_name: data.first_name,
-            last_name: data.last_name,
-            email: data.email
-          }
-        },
-        function(response, messages) {
-          if (response.status == 201) {
-            // successful signup
-            const routeData = vm.$router.resolve({
-              path: `/newsletter_status/confirm_${vm.lang}`
-            });
-            window.open(routeData.href, "_blank");
-          } else if (response.status == 200) {
-            // duplicate subscription
-            const routeData = vm.$router.resolve({
-              path: `/newsletter_status/duplicatemail_${vm.lang}`
-            });
-            window.open(routeData.href, "_blank");
-          } else {
-            // error while subscribing
-            const routeData = vm.$router.resolve({
-              path: `/newsletter_status/error_${vm.lang}`
-            });
-            window.open(routeData.href, "_blank");
-          }
-        },
-        function(response, messages) {
-          // error while subscribing
-          const routeData = vm.$router.resolve({
-            path: `/newsletter_status/error_${vm.lang}`
-          });
-          window.open(routeData.href, "_blank");
-        }
-      );
+    handleSubscribe(data) {
+      this.subscribe(data, this.content[this.lang]["register"]["token"])
     }
   }
 };
