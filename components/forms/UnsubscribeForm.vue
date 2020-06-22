ea80c1d6f8c162886653d546d0a7cdc307f10c18
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2020-06-22 13:18:17 +0200

    feat(newsletter): build universal unsubscribe form

---
+++ components/forms/UnsubscribeForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <form
+      method="POST"
+      v-on:submit.prevent="handleSubmit()"
+    >
+      <label>{{ this.content['mail'] }}</label>
+      <input
+        type="email"
+        name="email"
+        :placeholder="this.content['mail']"
+        v-model="email"
+        required
+      />
+      <input class="button is-color-secondary is-medium" type="submit" :value="this.content['button']" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UnsubscribeForm",
+  data() {
+    return {
+      email: null
+    }
+  },
+  props: ["content"],
+  methods: {
+    handleSubmit: function(e) {
+      this.$emit("submit-data", {
+        email: this.email
+      });
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  @import "../../assets/style/style.scss";
+
+  form {
+    width: 100%;
+    height: 100%;
+    font-size: $size-medium;
+
+    & > * {
+      margin-top: $spacing-m;
+    }
+
+    label {
+      display: none;
+    }
+
+    input {
+      background: $color-primary--lightest;
+      border: none;
+      font-size: $size-medium;
+      &[type=text], &[type=email] {
+        width: 100%;
+        padding: $spacing-s;
+        color: $text;
+      }
+    }
+  }
+</style>
