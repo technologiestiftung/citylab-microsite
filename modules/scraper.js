6671579c2545a294cdcb4c74cc2f29823c4f37ff
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-07-15 17:49:08 +0200

    add scraper plus test

--- modules/scraper.js
+++ modules/scraper.js
@@ -2,12 +2,23 @@ import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
 
-const urls = [
-  "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/3/public/values?alt=json",
+export const sheetUrls = [
+  "https://spreadsheets.google.com/feeds/list/1OB2kDr4rAyGZ_LuntV1ao7FeA4_vZgP95arR5RGk7M4/od6/public/values?alt=json",
   "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/1/public/values?alt=json",
-  "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/2/public/values?alt=json"
+  "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/2/public/values?alt=json",
+  "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/3/public/values?alt=json",
+  "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/4/public/values?alt=json",
+  "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/5/public/values?alt=json",
+  "https://spreadsheets.google.com/feeds/list/1rTyfInS6NjTifbru61mWEqICyv9uuMVSSk7NZTABLQc/2/public/values?alt=json",
 ];
 
+/**
+ * @param {string} url
+ * @returns {string} refactored url
+ */
+export function urlRefactor(url) {
+  return url.replace(/\.|\/|\?|=|&|:/g, "-");
+}
 async function fetchData(url) {
   try {
     const response = await fetch(url);
@@ -22,19 +33,20 @@ async function fetchData(url) {
     throw error;
   }
 }
-export default async function scraper(moduleOptions) {
+export default async function scraper(_moduleOptions) {
   try {
-    console.log("scraper module");
+    // console.log("scraper module");
     const data = {};
-    for (const url of urls) {
+    for (const url of sheetUrls) {
       const json = await fetchData(url);
-      data[url.replace(/\/|\?|\=|\&|:/g, "-")] = json;
+      data[urlRefactor(url)] = json;
     }
     fs.writeFileSync(
       path.resolve(__dirname, "../static/data/spreadsheets.json"),
       JSON.stringify(data)
     );
   } catch (error) {
+    console.error(error);
     throw error;
   }
 }
