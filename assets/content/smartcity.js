7f65cf490c0dd13605fa80794e179d0ef281a560
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-09-10 12:06:45 +0200

    first draft smart city page

---
+++ assets/content/smartcity.js
@@ -0,0 +1,69 @@
+export const smartcity = {
+  hero: {
+    title: {
+      en: "Smart City",
+      de: "Smart City",
+    },
+    subTitle: {
+      en: "foo bah of the future",
+      de: "Foo Bah der Zukunft",
+    },
+    subSubTitle: {
+      en: "even more text coming here even more and more and more",
+      de: "noch mehr text hier und mehr und mehr und mehr",
+    },
+    img: {
+      url: "/images/smartcity/smart-city_hero1.jpg",
+      alt: {
+        en: "hero image",
+        de: "hero bild",
+      },
+    },
+  },
+  title: {
+    en: "smart city not here",
+    de: "Smart City nicht hero",
+  },
+  subtitle: {
+    en: "Smart city subtitle not here",
+    de: "Smart City Sub-title nicht hero",
+  },
+  description: {
+    en:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    de:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  downloads: {
+    title: {
+      de: "Hier das PDF",
+      en: "Go grab it",
+    },
+    description: {
+      en:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      de:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    main: {
+      text: {
+        en: "The PDF",
+        de: "Das PDF",
+        url: "#",
+      },
+      img: {
+        url: "/images/smartcity/document.svg",
+        alt: {
+          en: "PDF Image",
+          de: "PDF Bild",
+        },
+      },
+    },
+    additional: [
+      { text: { en: "additional 1", de: "zusätzlich 1" }, url: "#" },
+      { text: { en: "additional 2", de: "zusätzlich 2" }, url: "#" },
+      { text: { en: "additional 3", de: "zusätzlich 3" }, url: "#" },
+      { text: { en: "additional 4", de: "zusätzlich 4" }, url: "#" },
+    ],
+  },
+};
