225e3b4e511b6d348e45e03275b8c108d3d9cd4e
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-09-11 10:58:02 +0200

    Typos and links in the text

--- assets/content/smartcity.js
+++ assets/content/smartcity.js
@@ -5,8 +5,8 @@ export const smartcity = {
       de: "Berlin wird Modellprojekt Smart City!",
     },
     subTitle: {
-      en: "Auf dem Weg zu einer neuen Smart City-Strategie",
-      de: "Auf dem Weg zu einer neuen Smart City-Strategie",
+      en: "Auf dem Weg zu einer neuen Smart-City-Strategie",
+      de: "Auf dem Weg zu einer neuen Smart-City-Strategie",
     },
     button: {
       label: {
@@ -21,8 +21,8 @@ export const smartcity = {
       de: "Berlin wird Modellprojekt Smart City!",
     },
     subTitle: {
-      en: "Auf dem Weg zu einer neuen Smart City-Strategie",
-      de: "Auf dem Weg zu einer neuen Smart City-Strategie",
+      en: "Auf dem Weg zu einer neuen Smart-City-Strategie",
+      de: "Auf dem Weg zu einer neuen Smart-City-Strategie",
     },
     subSubTitle: {
       en: "",
@@ -45,27 +45,27 @@ export const smartcity = {
     de: "",
   },
   description: {
-    en:
-    "Das Land Berlin hat sich erfolgreich in der 2. Förderstaffel als „Modellprojekt Smart City“ beim Bundesministerium des Innern, für Bau und Heimat (BMI) beworben. Das Motto der Förderstaffel lautet „Gemeinwohl und Netzwerkstadt/Stadtnetzwerk“. Unter dieser Leitschnur wird Berlin nun eine neue Smart-City-Strategie erarbeiten und fünf Umsetzungsprojekte generieren. <br /> Wir freuen uns, als CityLAB diesen Prozess aktiv zu begleiten. Gemeinsam mit unserer Community haben wir im letzten Jahr zahlreiche spannende Projekte realisiert und dabei vielfältige Erfahrungen gesammelt, die natürlich in die neue Smart City-Strategie einfließen sollen. Um den ganzen Prozess möglichst transparent und partizipativ zu gestalten, werden wir in den nächsten Monaten verschiedene Beteiligungsformate anbieten. Als ersten Schritt veröffentlichen wir hier die Antragsunterlagen.",
-    de:
-    "Das Land Berlin hat sich erfolgreich in der 2. Förderstaffel als „Modellprojekt Smart City“ beim Bundesministerium des Innern, für Bau und Heimat (BMI) beworben. Das Motto der Förderstaffel lautet „Gemeinwohl und Netzwerkstadt/Stadtnetzwerk“. Unter dieser Leitschnur wird Berlin nun eine neue Smart-City-Strategie erarbeiten und fünf Umsetzungsprojekte generieren. <br /> Wir freuen uns, als CityLAB diesen Prozess aktiv zu begleiten. Gemeinsam mit unserer Community haben wir im letzten Jahr zahlreiche spannende Projekte realisiert und dabei vielfältige Erfahrungen gesammelt, die natürlich in die neue Smart City-Strategie einfließen sollen. Um den ganzen Prozess möglichst transparent und partizipativ zu gestalten, werden wir in den nächsten Monaten verschiedene Beteiligungsformate anbieten. Als ersten Schritt veröffentlichen wir hier die Antragsunterlagen.",
+    en: `Das Land Berlin hat sich erfolgreich in der 2. Förderstaffel als „Modellprojekt Smart City“ beim Bundesministerium des Innern, für Bau und Heimat (BMI) beworben. Das Motto der Förderstaffel lautet „Gemeinwohl und Netzwerkstadt/Stadtnetzwerk“. Unter dieser Leitschnur wird Berlin nun eine neue Smart-City-Strategie erarbeiten und fünf Umsetzungsprojekte generieren. (<a rel="noopener noreferrer" target="_blank" href="https://www.berlin.de/rbmskzl/aktuelles/pressemitteilungen/2020/pressemitteilung.988471.php">Presemitteilung</a>) <br />
+Wir freuen uns, als CityLAB diesen Prozess aktiv zu begleiten. Gemeinsam mit unserer Community haben wir im letzten Jahr zahlreiche spannende Projekte realisiert und dabei vielfältige Erfahrungen gesammelt, die natürlich in die neue Smart City-Strategie einfließen sollen. Um den ganzen Prozess möglichst transparent und partizipativ zu gestalten, werden wir in den nächsten Monaten verschiedene Beteiligungsformate anbieten. Als ersten Schritt veröffentlichen wir hier die Antragsunterlagen.`,
+    de: `Das Land Berlin hat sich erfolgreich in der 2. Förderstaffel als „Modellprojekt Smart City“ beim Bundesministerium des Innern, für Bau und Heimat (BMI) beworben. Das Motto der Förderstaffel lautet „Gemeinwohl und Netzwerkstadt/Stadtnetzwerk“. Unter dieser Leitschnur wird Berlin nun eine neue Smart-City-Strategie erarbeiten und fünf Umsetzungsprojekte generieren. (<a rel="noopener noreferrer" target="_blank" href="https://www.berlin.de/rbmskzl/aktuelles/pressemitteilungen/2020/pressemitteilung.988471.php">Presemitteilung</a>) <br />
+Wir freuen uns, als CityLAB diesen Prozess aktiv zu begleiten. Gemeinsam mit unserer Community haben wir im letzten Jahr zahlreiche spannende Projekte realisiert und dabei vielfältige Erfahrungen gesammelt, die natürlich in die neue Smart City-Strategie einfließen sollen. Um den ganzen Prozess möglichst transparent und partizipativ zu gestalten, werden wir in den nächsten Monaten verschiedene Beteiligungsformate anbieten. Als ersten Schritt veröffentlichen wir hier die Antragsunterlagen.`,
   },
   downloads: {
     title: {
-      de: "<- Antrag",
-      en: "<- Download",
+      de: "Antrag runterladen",
+      en: "Download",
     },
     description: {
       en:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "The application was drafted by a consortium of civil society, business, science and administration under the leadership of the Berlin Senate Chancellery.",
       de:
         "Der Antrag wurde von einem Konsortium aus Zivilgesellschaft, Wirtschaft, Wissenschaft und Verwaltung unter Federführung der Berliner Senatskanzlei verfasst.",
     },
     main: {
+      url: "/docs/Antrag_Modellprojekte_SC_Berlin.pdf",
       text: {
         en: "The PDF",
         de: "Antragsdokument",
-        url: "../../docs/Antrag_Modellprojekte_SC_Berlin.pdf",
       },
       img: {
         url: "/images/smartcity/document.svg",
@@ -76,10 +76,22 @@ export const smartcity = {
       },
     },
     additional: [
-      { text: { en: "additional 1", de: "Anhang 1: Kommunikation" }, url: "../../docs/SC_Kommunikationsstruktur.pdf" },
-      { text: { en: "additional 2", de: "Anhang 2: Raumbezüge " }, url: "../../docs/SC_Raumbezüge.pdf" },
-      { text: { en: "additional 3", de: "Anhang 3: Steuerung" }, url: "../../docs/SC_Steuerung.pdf" },
-      { text: { en: "additional 4", de: "Anhang 4: Vorhaben" }, url: "../../docs/SC_Vorhaben.pdf" },
+      {
+        text: { en: "additional 1", de: "Anhang 1: Kommunikation" },
+        url: "/docs/SC_Kommunikationsstruktur.pdf",
+      },
+      {
+        text: { en: "additional 2", de: "Anhang 2: Raumbezüge " },
+        url: "/docs/SC_Raumbezüge.pdf",
+      },
+      {
+        text: { en: "additional 3", de: "Anhang 3: Steuerung" },
+        url: "/docs/SC_Steuerung.pdf",
+      },
+      {
+        text: { en: "additional 4", de: "Anhang 4: Vorhaben" },
+        url: "/docs/SC_Vorhaben.pdf",
+      },
     ],
   },
 };
