a180daf90a662faa7d4a76676d2ed8bc45aeb74f
Fabian Morón Zirfas <moron-zirfas@technologiestiftung-berlin.de>
2020-09-15 18:31:30 +0200

    Feature/exhibition note (#139)

---
+++ components/Announcements.vue
@@ -0,0 +1,37 @@
+<template>
+  <section v-if="renderIt" id="announcement" class="section">
+    <div class="container">
+      <h2 class="subtitle announcement__title">{{ title }}</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import { content } from "../assets/content";
+export default {
+  props: {
+    lang: String,
+  },
+  computed: {
+    renderIt() {
+      return content.announcements ? true : false;
+    },
+    title() {
+      return content.announcements.title[this.lang];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/style.scss";
+
+.section#announcement {
+  background-color: #2f2fa2;
+}
+.announcement__title {
+  color: white !important;
+  font-size: 1.6rem !important;
+  text-align: center;
+}
+</style>
--- components/TeaserIntro.vue
+++ components/TeaserIntro.vue
@@ -1,231 +1,237 @@
 <template>
-    <section class="section intro">
+  <section class="section intro">
+    <div class="preview container" :id="`preview-${id}`">
+      <div class="wrapper">
+        <!-- <h2 class="title" v-html="contentTeaser['title']"></h2> -->
+        <vue-typed-js
+          :strings="contentTeaser['words_arr']"
+          :loop="true"
+          :typeSpeed="100"
+          :backDelay="4000"
+        >
+          <h1 style="width: 100%;" class="title">
+            {{ contentTeaser["claim_01"] }}
+            <span class="typing"></span>
+            {{ contentTeaser["claim_02"] }}
+            <br class="condBreak" />
+            {{ contentTeaser["claim_03"] }}
+          </h1>
+        </vue-typed-js>
+        <h2 v-html="contentTeaser['description']" class="subtitle"></h2>
 
-        <div class="preview container" :id="`preview-${id}`" >
-            <div class="wrapper">
-                <!-- <h2 class="title" v-html="contentTeaser['title']"></h2> -->
-                <vue-typed-js
-                    :strings="contentTeaser['words_arr']" 
-                    :loop="true"
-                    :typeSpeed="100"
-                    :backDelay="4000"
-                >
-                    <h1 style="width: 100%;" class="title">
-                        {{ contentTeaser['claim_01'] }}
-                        <span class="typing"></span>
-                        {{ contentTeaser['claim_02'] }}
-                        <br class="condBreak"/>
-                        {{ contentTeaser['claim_03'] }}
-                    </h1>
-                </vue-typed-js>
-                <h2 v-html="contentTeaser['description']" class="subtitle"></h2>
+        <Button
+          style="margin-bottom: 10px;"
+          :label="contentTeaser['btn']"
+          :direct="contentTeaser['direct']"
+        />
 
-                <Button style="margin-bottom: 10px;" :label="contentTeaser['btn']" :direct="contentTeaser['direct']"/>
+        <nuxt-link
+          class="cross-link--small"
+          :to="contentTeaser['directSecond']"
+        >
+          {{ contentTeaser["btnSecond"] }}
+        </nuxt-link>
+      </div>
+      <v-lazy-image
+        class="lazy-img"
+        :src="contentTeaser['imgUrl']"
+        :src-placeholder="contentTeaser['imgUrlLazy']"
+      />
+      <img :src="contentTeaser['imgUrl']" />
+    </div>
 
-                <nuxt-link class="cross-link--small" :to="contentTeaser['directSecond']">
-                    {{ contentTeaser['btnSecond'] }}
-                </nuxt-link>
-            </div>
-            <v-lazy-image
-                class="lazy-img"
-                :src="contentTeaser['imgUrl']"
-                :src-placeholder="contentTeaser['imgUrlLazy']"
-            />
-            <img :src="contentTeaser['imgUrl']"/>
-        </div>
+    <div class="wrapper-mobile is-medium">
+      <!-- <h2 v-html="contentTeaser['title']" class="title"></h2> -->
+      <vue-typed-js
+        :strings="contentTeaser['words_arr']"
+        :loop="true"
+        :typeSpeed="100"
+        :backDelay="4000"
+      >
+        <h1 style="width: 100%;" class="title">
+          {{ contentTeaser["claim_01"] }}
+          <span class="typing"></span>
+          {{ contentTeaser["claim_02"] }}
+          <br class="condBreak" />
+          {{ contentTeaser["claim_03"] }}
+        </h1>
+      </vue-typed-js>
 
-        <div class="wrapper-mobile is-medium">
+      <h2 v-html="contentTeaser['description']" class="subtitle"></h2>
 
-            <!-- <h2 v-html="contentTeaser['title']" class="title"></h2> -->
-            <vue-typed-js
-                :strings="contentTeaser['words_arr']"
-                :loop="true"
-                :typeSpeed="100"
-                :backDelay="4000"
-            >
-                <h1 style="width: 100%;" class="title">
-                    {{ contentTeaser['claim_01'] }}
-                    <span class="typing"></span>
-                    {{ contentTeaser['claim_02'] }}
-                    <br class="condBreak"/>
-                    {{ contentTeaser['claim_03'] }}
-                </h1>
-            </vue-typed-js>
+      <Button
+        style="margin-bottom: 10px;"
+        :label="contentTeaser['btn']"
+        :direct="contentTeaser['direct']"
+      />
 
-            <h2 v-html="contentTeaser['description']" class="subtitle"></h2>
-
-            <Button style="margin-bottom: 10px;" :label="contentTeaser['btn']" :direct="contentTeaser['direct']"/>
-
-            <nuxt-link class="cross-link--small" :to="contentTeaser['directSecond']">
-                {{ contentTeaser['btnSecond'] }}
-            </nuxt-link>
-        </div>
-    </section>
+      <nuxt-link class="cross-link--small" :to="contentTeaser['directSecond']">
+        {{ contentTeaser["btnSecond"] }}
+      </nuxt-link>
+    </div>
+  </section>
 </template>
 
 <script>
-    import VueTypedJs from 'vue-typed-js';
-    import Vue from 'vue';
-    import Button from './Button.vue'
-    import VLazyImage from "v-lazy-image";
+import VueTypedJs from "vue-typed-js";
+import Vue from "vue";
+import Button from "./Button.vue";
+import VLazyImage from "v-lazy-image";
 
-    Vue.use(VueTypedJs);
-    export default {
-        props: {
-          lang: String,
-          content: Object,
-          direct: String,
-          topic: String,
-          subtopic: String,
-          id: String
-        },
-        components: {
-          Button,
-          VLazyImage
-        },
-        name: 'Teaser',
-        computed: {
-            contentTeaser: function() { return this.content[this.lang][this.topic][this.subtopic] },
-        },
-        methods: {
-            adjustHeight() {
-                const elm = document.getElementById(`preview-${this.id}`);
-                const height = (elm.offsetWidth / 100) * 56.9;
-                elm.style.height = `${height}px`;
-            },
-
-        },
-        mounted() {
-            this.$nextTick(function () {
-                this.adjustHeight();
-            })
-            window.addEventListener('resize', this.adjustHeight)
-        },
-    }
+Vue.use(VueTypedJs);
+export default {
+  props: {
+    lang: String,
+    content: Object,
+    direct: String,
+    topic: String,
+    subtopic: String,
+    id: String,
+  },
+  components: {
+    Button,
+    VLazyImage,
+  },
+  name: "Teaser",
+  computed: {
+    contentTeaser: function () {
+      return this.content[this.lang][this.topic][this.subtopic];
+    },
+  },
+  methods: {
+    adjustHeight() {
+      const elm = document.getElementById(`preview-${this.id}`);
+      const height = (elm.offsetWidth / 100) * 56.9;
+      elm.style.height = `${height}px`;
+    },
+  },
+  mounted() {
+    this.$nextTick(function () {
+      this.adjustHeight();
+    });
+    window.addEventListener("resize", this.adjustHeight);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    @import "../assets/style/style.scss";
-    .section {
-        padding: 3.5rem 0rem;
+@import "../assets/style/style.scss";
+.section {
+  padding: 3.5rem 0rem;
 
-        &.intro {
-            margin-top: 19px;
+  &.intro {
+    margin-top: 19px;
 
-            @media screen and (max-width: 850px) {
-                margin-top: 11px;
-            }
-        }
+    @media screen and (max-width: 850px) {
+      margin-top: 11px;
+    }
+  }
+}
+
+.lazy-img {
+  width: 100% !important;
+  height: auto;
+}
+
+.cross-link--small {
+  border-bottom: 1px solid $color-secondary;
+  padding-bottom: 5px;
+  color: $color-secondary;
+
+  &:hover {
+    color: $color-secondary--medium;
+    border-bottom: 1px solid $color-secondary--medium;
+  }
+}
+
+.container {
+  animation: sweep 0.5s ease-in-out;
+
+  &.preview {
+    display: grid;
+    grid-template-columns: repeat(16, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    max-width: 1200px;
+
+    img {
+      grid-column: 1 / span 16;
+      grid-row: 1 / span 3;
+
+      width: auto;
+
+      overflow: hidden;
     }
 
-    .lazy-img {
-        width: 100% !important;
-        height: auto;
-    }
+    div.wrapper {
+      background: white;
+      padding: 1.75rem 2rem 2.5rem 2rem;
 
-    .cross-link--small {
-        border-bottom: 1px solid $color-secondary;
-        padding-bottom: 5px;
+      grid-column: 3 / 8;
+      grid-row: 2;
+
+      align-self: center;
+      justify-self: center;
+
+      z-index: 1;
+
+      h1.title {
         color: $color-secondary;
+        margin-bottom: 30px;
+      }
 
-        &:hover {
-            color: $color-secondary--medium;
-            border-bottom: 1px solid $color-secondary--medium;
-        }
-    }
+      h2.subtitle {
+        margin-top: 1rem;
+        margin-bottom: 0.5rem;
+      }
 
-    .container {
-        animation: sweep .5s ease-in-out;
+      @media screen and (max-width: 1100px) {
+        grid-column: 2 / 9;
+      }
 
-        &.preview {
-            display: grid;
-            grid-template-columns: repeat(16, 1fr);
-            grid-template-rows: repeat(3, 1fr);
-            max-width: 1200px;
+      @media screen and (max-width: 1000px) {
+        grid-column: 2 / 10;
+      }
 
-            img {
-                grid-column: 1 / span 16;
-                grid-row: 1 / span 3;
-
-                width: auto;
-
-                overflow: hidden;
-            }
-
-            div.wrapper {
-                background: white;
-                padding: 1.75rem 2rem 2.5rem 2rem;
-
-                grid-column: 3 / 8;
-                grid-row: 2;
-
-                align-self: center;
-                justify-self: center;
-
-                z-index: 1;
-
-
-                h1.title {
-                  color: $color-secondary;
-                  margin-bottom: 30px;
-                }
-
-                h2.subtitle {
-                  margin-top: 1rem;
-                  margin-bottom: .5rem;
-                }
-
-                @media screen and (max-width: 1100px) {
-                    grid-column: 2 / 9;
-                }
-
-                @media screen and (max-width: 1000px) {
-                    grid-column: 2 / 10;
-                }
-
-                @media screen and (max-width: 850px) {
-                    display: none;
-                }
-            }
-        }
-    }
-
-    .preview {
-        max-width: 1200px;
-        height: auto;
-    }
-
-    .wrapper-mobile {
+      @media screen and (max-width: 850px) {
         display: none;
+      }
+    }
+  }
+}
 
-        h2.title {
-          color: $color-secondary;
-        }
+.preview {
+  max-width: 1200px;
+  height: auto;
+}
 
-        @media screen and (max-width: 850px) {
+.wrapper-mobile {
+  display: none;
 
-            h1.title {
-                color: $color-secondary !important;
-                margin-bottom: 1.5rem;
-            }
+  h2.title {
+    color: $color-secondary;
+  }
 
-            display: block;
-            padding: 1.5rem 1.5rem 0rem 1.5rem;
-        }
+  @media screen and (max-width: 850px) {
+    h1.title {
+      color: $color-secondary !important;
+      margin-bottom: 1.5rem;
     }
 
-    .cross-link {
-        padding-bottom: 10px;
-        font-size: 24px;
-        border-bottom: 1px solid;
-        color: #393A60;
-        cursor: pointer;
-        color: rgb(246, 76, 114);
+    display: block;
+    padding: 1.5rem 1.5rem 0rem 1.5rem;
+  }
+}
 
-        &:hover {
-            color: rgb(243, 125, 150);
-        }
-    }
+.cross-link {
+  padding-bottom: 10px;
+  font-size: 24px;
+  border-bottom: 1px solid;
+  color: #393a60;
+  cursor: pointer;
+  color: rgb(246, 76, 114);
+
+  &:hover {
+    color: rgb(243, 125, 150);
+  }
+}
 </style>
-
-
