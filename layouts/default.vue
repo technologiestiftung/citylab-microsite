3597871ff9e2dd0f3860d99e550397da5a2e5869
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2021-08-17 13:21:31 +0200

    chore: add note for spreadsheet issue [hotfix]

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
     <nuxt />
+    <div class="technical-issues-note">
+      Aufgrund von Wartungsarbeiten werden einige Seiten momentan nicht korrekt
+      dargestellt. Wir arbeiten an einer Lösung. Aktuelle Veranstaltungshinweise
+      findet ihr auf unseren
+      <a
+        href="https://twitter.com/citylabberlin"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Twitter</a
+      >
+      - und
+      <a
+        href="https://www.instagram.com/citylabbln/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Instagram</a
+      >-Profilen.
+    </div>
   </div>
 </template>
 
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -51,5 +69,27 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+.technical-issues-note {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  padding: 12px;
+  background-color: #2f2fa2;
+  color: white;
+  z-index: 9999;
+  font-size: 0.9rem;
+  border-top: 2px solid white;
+}
+
+.technical-issues-note a {
+  color: white;
+  text-decoration: underline;
+}
+
+.technical-issues-note a:hover {
+  color: white;
 }
 </style>
